a7050ca28a61696d49af485f86576f95257ce198
Sergi Mauri Curto <sergimauri8@gmail.com>
2016-09-16 16:28:56 +0200

    refactoritzacio

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alumne
+ * Date: 16/09/16
+ * Time: 15:53
+ */
+
+$names = ['Sergi', 'Xavi', 'Joan'];
+
+    foreach ($names as $name) {
+        echo "name";
+    }
+&person = [
+    'name' => 'Sergi'
+    'sn1' => 'Mauri'
+]
+
+
+echo $names;
+
+
+//require "index.template.php";
