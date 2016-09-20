abd6cd6e5241c073adb01254b8b41962e12c5f28
Sergi Mauri Curto <sergimauri8@gmail.com>
2016-09-20 21:11:19 +0200

    Introduccio a les classes (Programacio orientada a objectes)

--- index.php
+++ index.php
@@ -5,38 +5,22 @@ require "functions.php";
 require "task.php";
 
 
+
 try {
     $pdo = new PDO('mysql:HOST=127.0.0.1;dbname=prova', 'root','');
-
+    return $pdo;
     }
     catch (PDOException $a){
         die("Ha hagut un error durant la connexió. Missatge: " . $a->getMessage());
 
     }
 
-    
+
 $query = $pdo->prepare('SELECT * FROM todos');
 
 $query->execute();
 
-//var_dump($query->fetchAll(PDO::FETCH_OBJ));
-
-//var_dump($query->fetchAll(PDO::FETCH_OBJ)[0]->description);
-
 $tasks = $query->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,task::class);
-//$task1 = $results[0];
-
-//var_dump($task1->id);
-//var_dump($results[0]->id);
-
-//var_dump($task1->description);
-//var_dump($task1->completed);
-
-//var_dump($query->fetchAll(PDO::FETCH_CLASS,task::class));
 
 
-
-
-
-
-//require "index.template.php";
+require "index.template.php";
