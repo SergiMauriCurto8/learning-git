164aca6ffb8ed987559df2f6f02b270817abd238
Sergi Mauri Curto <sergimauri8@gmail.com>
2016-09-20 21:26:26 +0200

    Introduccio a les classes (Programacio orientada a objectes)

--- index.php
+++ index.php
@@ -4,12 +4,7 @@ require "functions.php";
 
 require "task.php";
 
-$connection = new connection();
-$pdo = $connection->make();
-
 $pdo = connection::make();
-
-$pdo = connect();
 
 $tasks = allTasks($pdo);
 
