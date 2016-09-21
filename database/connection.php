fa90f77c6755bb7c9c47e5dd7db2d12c1d710fd4
Sergi Mauri Curto <sergimauri8@gmail.com>
2016-09-21 19:44:19 +0200

    database file

--- database/connection.php
+++ database/connection.php
@@ -1,18 +1,32 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: alumne
- * Date: 20/09/16
- * Time: 21:23
- */
+$config = (
+    'type' => 'mysql',
+    'name' => 'prova',
+    'username' => 'root',
+    'password' => '',
+    'host' => '127.0.0.1'
+);
+
+
 class connection
 {
 
     public static function make()
     {
         try {
-            $pdo = new PDO('mysql:HOST=127.0.0.1;dbname=prova', 'root', '');
+            //$pdo = new PDO('mysql:HOST=127.0.0.1;dbname=prova', 'root', '');
+            var_dump($config['dbtype'] . ':host=',
+                $config['dbhost'] . ':dbname=',
+                $config['dbname']);
+
+            $pdo = new PDO(
+                $config['dbtype'] . ':host=',
+                $config['dbhost'] . ':dbname=',
+                $config['dbtype'],
+                $config['dbtype'],
+                $config['dbtype']);
+
             return $pdo;
         } catch (PDOException $a) {
             die("Ha hagut un error durant la connexió. Missatge: " . $a->getMessage());
