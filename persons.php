b12248121788013f89d7c3479103b6815b70194b
Sergi Mauri Curto <sergimauri8@gmail.com>
2016-09-22 20:57:08 +0200

    Nou Index anomenat persons

--- persons.php
+++ persons.php
@@ -1,23 +1,11 @@
 <?php
 
-require "functions.php";
+require 'bootstrap.php'
 
-require "task.php";
-
-require 'database/connection.php';
-
-require 'database/QueryBuilder.php';
-
-$config = require 'config/database.php';
-
-$message = require 'config/message.php';
-
-$pdo = connection::make($config,$message);
-
-$query = new QueryBuilder($pdo);
 $tasks = $query->all('persons');
 
 require "index.template.php";
 
 //DRY: DON'T REPEAT YOURSELF
 //WET: WRITE EVERITHING TWICE
+
