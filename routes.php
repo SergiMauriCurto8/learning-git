044731f7f0737bb32850b58648d3b951215bfcea
Sergi Mauri Curto <sergimauri8@gmail.com>
2016-10-04 18:30:12 +0200

    Versio final

---
+++ routes.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    ''           => 'controllers/tasks.php',
+    'persons'    => 'controllers/persons.php',
+    'about'      => 'controllers/about.php',
+    'contact'    => 'controllers/contact.php',
+    'addperson' => 'controllers/addperson.php',
+    'names'      => 'controllers/names.php',
+];
